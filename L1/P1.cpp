1644c18a9bd0d934b4b6d415de396a2dd7e1d4ad
sanahopk <ordo@localhost.localdomain>
2024-10-18 00:44:55 +0300

    Some formatting

--- L1/P1.cpp
+++ L1/P1.cpp
@@ -1,41 +1,41 @@
-#include <iostream>
 #include <cstring>
+#include <iostream>
 
 using namespace std;
 
 string reInit(char input[], int length) {
-    string result = "";
-    int count = 0;
+  string result = "";
+  int count = 0;
 
-    for (int i = 0; i < length; ++i) {
-        if (i > 0 && input[i] == input[i - 1]) {
-            ++count;
-        } else {
-            count = 1;
-        }
-
-        if (count == 1) {
-            result += input[i];
-        } else if (count > 3) {
-			result.pop_back();
-            result += "###";
-        }
+  for (int i = 0; i < length; ++i) {
+    if (i > 0 && input[i] == input[i - 1]) {
+      ++count;
+    } else {
+      count = 1;
     }
 
-    return result;
+    if (count == 1) {
+      result += input[i];
+    } else if (count > 3) {
+      result.pop_back();
+      result += "###";
+    }
+  }
+
+  return result;
 }
 
 int main() {
-	int n;
-	char str[100];
-	char temp;
-	cout << "Enter the array: " << endl;
-	temp = getchar();
-	while(temp!='\n') {		
-		str[n]=temp;
-		n++;
-		temp=getchar();
-	}
-	string res = reInit(str, n);
-	cout << res << endl;
+  int n;
+  char str[100];
+  char temp;
+  cout << "Enter the array: " << endl;
+  temp = getchar();
+  while (temp != '\n') {
+    str[n] = temp;
+    n++;
+    temp = getchar();
+  }
+  string res = reInit(str, n);
+  cout << res << endl;
 }
--- L1/P2.cpp
+++ L1/P2.cpp
@@ -1,36 +1,36 @@
-#include <iostream>
 #include <cstring>
+#include <iostream>
 
 using namespace std;
 
 string reInit(char input[], int length) {
-    string result = "";
-    int count = 0;
+  string result = "";
+  int count = 0;
 
-    for (int i = 0; i < length; ++i) {
-        if (i > 0 && input[i] == input[i - 1]) {
-            ++count;
-        } else {
-            count = 1;
-        }
-
-        if (count == 1) {
-            result += input[i];
-        } else if (count > 3) {
-			result.pop_back();
-            result += "###";
-        }
+  for (int i = 0; i < length; ++i) {
+    if (i > 0 && input[i] == input[i - 1]) {
+      ++count;
+    } else {
+      count = 1;
     }
 
-    return result;
+    if (count == 1) {
+      result += input[i];
+    } else if (count > 3) {
+      result.pop_back();
+      result += "###";
+    }
+  }
+
+  return result;
 }
 
 int main() {
-	char str[100];
-	int n;
-	cout << "Enter the array: " << endl;
-	cin >> str;
-	n = strlen(str);
-	string res = reInit(str, n);
-	cout << res << endl;
+  char str[100];
+  int n;
+  cout << "Enter the array: " << endl;
+  cin >> str;
+  n = strlen(str);
+  string res = reInit(str, n);
+  cout << res << endl;
 }
