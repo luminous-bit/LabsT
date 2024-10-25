01ac306a11e98e97f3a194d70afe0b4ca68370dd
sanahopk <ordo@localhost.localdomain>
2024-10-25 12:56:03 +0300

    Checkin' in some tasks...

---
+++ L2/P1.c
@@ -0,0 +1,94 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char* read_string() {
+  char* str = NULL;
+  size_t size = 0;
+  size_t len = 0;
+  char c;
+
+  while ((c = getchar()) != '\n') {
+    size_t new_size = size + 1;
+    char* new_str = (char*)malloc(new_size * sizeof(char));
+
+    if (new_str == NULL) {
+      printf("Memory allocation failed!\n");
+      if (str != NULL) free(str);
+      return NULL;
+    }
+
+    if (size > 0) {
+      memcpy(new_str, str, len);
+      free(str);
+    }
+
+    new_str[len] = c;
+    new_str[len + 1] = '\0';
+    str = new_str;
+    size = new_size;
+    len++;
+  }
+
+  return str;
+}
+
+void add_char_to_string(char* my_string, char character_to_add) {
+  char char_as_string[2] = {character_to_add, '\0'};
+
+  strcat(my_string, char_as_string);
+}
+
+char* remove_last_char(char* str) {
+  if (str == NULL || strlen(str) == 0) {
+    return str;
+  }
+
+  size_t len = strlen(str);
+
+  char* new_str = (char*)malloc((len - 1) * sizeof(char));
+  if (new_str == NULL) {
+    printf("Memory allocation failed!\n");
+    return NULL;
+  }
+
+  strncpy(new_str, str, len - 1);
+
+  new_str[len - 1] = '\0';
+
+  return new_str;
+}
+
+char* reInit(char* input, char* result) {
+  int length = strlen(input);
+  int count = 0;
+  int done = 0;
+  char addition[] = "###";
+
+  for (int i = 0; i < length; ++i) {
+    if (i > 0 && input[i] == input[i - 1]) {
+      ++count;
+    } else {
+      count = 1;
+    }
+
+    if (count == 1) {
+      add_char_to_string(result, input[i]);
+    } else if (count > 3 && input[i] != input[i + 1] && done != 1) {
+      result = remove_last_char(result);
+      strcat(result, addition);
+      done = 1;
+    }
+    done = 0;
+  }
+
+  return result;
+}
+
+int main() {
+  char result[1000] = {""};
+  printf("Enter the array: \n");
+  char* input_string = read_string();
+  char* fin = reInit(input_string, result);
+  printf("Final array: %s\n", fin);
+}
---
+++ L2/P2.c
@@ -0,0 +1,96 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void read_string(char** str, int* len, int increment) {
+  int capacity = 5;
+  *str = (char*)calloc(capacity, sizeof(char));
+  int done = 0;
+  if (*str == NULL) {
+    printf("Memory allocation failed!\n");
+  } else {
+    while (1) {
+      char c = getchar();
+
+      if (c != '\n' && c != EOF) {
+        if (*len == capacity) {
+          capacity += increment;
+          *str = (char*)realloc(*str, capacity * sizeof(char));
+          if (*str == NULL) {
+            printf("Memory allocation failed!\n");
+            break;
+            done = 1;
+          }
+        }
+        (*str)[(*len)++] = c;
+      } else {
+        break;
+      }
+    }
+
+    if (done == 0) (*str)[*len] = '\0';
+  }
+}
+
+void add_char_to_string(char* my_string, char character_to_add) {
+  char char_as_string[2] = {character_to_add, '\0'};
+
+  strcat(my_string, char_as_string);
+}
+
+char* remove_last_char(char* str) {
+  if (str == NULL || strlen(str) == 0) {
+    return str;
+  }
+
+  size_t len = strlen(str);
+
+  char* new_str = (char*)malloc((len - 1) * sizeof(char));
+  if (new_str == NULL) {
+    printf("Memory allocation failed!\n");
+    return NULL;
+  }
+
+  strncpy(new_str, str, len - 1);
+
+  new_str[len - 1] = '\0';
+
+  return new_str;
+}
+
+char* reInit(char* input, char* result) {
+  int length = strlen(input);
+  int count = 0;
+  int done = 0;
+  char addition[] = "###";
+
+  for (int i = 0; i < length; ++i) {
+    if (i > 0 && input[i] == input[i - 1]) {
+      ++count;
+    } else {
+      count = 1;
+    }
+
+    if (count == 1) {
+      add_char_to_string(result, input[i]);
+    } else if (count > 3 && input[i] != input[i + 1] && done != 1) {
+      result = remove_last_char(result);
+      strcat(result, addition);
+      done = 1;
+    }
+    done = 0;
+  }
+
+  return result;
+}
+
+int main() {
+  char* str = NULL;
+  char result[1000] = {""};
+  int len = 0;
+  int increment = 5;
+  printf("Enter the array: \n");
+  read_string(&str, &len, increment);
+  char* fin = reInit(str, result);
+  printf("Final array: %s\n", fin);
+}
